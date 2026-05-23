ae74698773971d8eee1cff3e9c0787f7a7cafb60
garakatsamol <tasosioan@gmail.com>
2026-05-24 00:02:12 +0300

    Add test for UploadMedia to verify 404 response for non-existent post

--- backend/LinkMeIn.Api.Tests/MediaControllerTests.cs
+++ backend/LinkMeIn.Api.Tests/MediaControllerTests.cs
@@ -12,6 +12,36 @@ namespace LinkMeIn.Api.Tests
     public class MediaControllerTests : IClassFixture<CustomWebApplicationFactory<Program>>
     {
         [Fact]
+        public async Task UploadMedia_NonExistentPost_Returns404()
+        {
+            await _factory.ResetDatabaseAsync();
+            var client = CreateClient();
+
+            var randomPostId = Guid.NewGuid();
+
+            // Create a small fake PNG image in memory
+            var imageBytes = new byte[] {
+                0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, // PNG header
+                0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52,
+                0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01,
+                0x08, 0x06, 0x00, 0x00, 0x00, 0x1F, 0x15, 0xC4,
+                0x89, 0x00, 0x00, 0x00, 0x0A, 0x49, 0x44, 0x41,
+                0x54, 0x78, 0x9C, 0x63, 0x60, 0x00, 0x00, 0x00,
+                0x02, 0x00, 0x01, 0xE2, 0x21, 0xBC, 0x33, 0x00,
+                0x00, 0x00, 0x00, 0x49, 0x45, 0x4E, 0x44, 0xAE,
+                0x42, 0x60, 0x82
+            };
+            var imageContent = new ByteArrayContent(imageBytes);
+            imageContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("image/png");
+
+            var form = new MultipartFormDataContent();
+            form.Add(imageContent, "file", "test.png");
+
+            // Upload image to non-existent post
+            var uploadResp = await client.PostAsync($"/api/posts/{randomPostId}/media", form);
+            Assert.Equal(HttpStatusCode.NotFound, uploadResp.StatusCode);
+        }
+        [Fact]
         public async Task UploadMedia_InvalidContentType_Returns400()
         {
             await _factory.ResetDatabaseAsync();
